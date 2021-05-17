2697aa3dd15d2166357ef5b788c4c647c4aa85c2
Rajendran <PraveenKumar.Rajendran@nielseniq.com>
2021-05-17 15:43:59 +0530

    email feature

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://rajenpk:Mapr4567@prawin-1pj0p.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true })
-
+const nodemailer = require("nodemailer");
 const app = express();
 
 app.use(express.static('public'));
@@ -19,5 +19,38 @@ app.get('/timestamp',(req,res)=>{
 			});
         });
 });
+
+const transporter = nodemailer.createTransport({
+  host: "gmail", //replace with your email provider
+  port: 587,
+  auth: {
+    user: 'rpk.sharaj@gmail.com',
+    pass: 'rpksharaj89',
+  },
+});
+
+
+app.post("/send", (req, res) => {
+    //2. You can configure the object however you want
+    const mailOptions = {
+     from: "rpk.sharaj@gmail.com",
+     to: "rpk.sharaj@gmail.com",
+     subject: "Node.js Email with Secure OAuth",
+     generateTextFromHTML: true,
+     html: "<b>test</b>"
+	};
+
+    //3.
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
+Let'
 
 app.listen(process.env.PORT || 8000,()=>{ console.log('server running');})
